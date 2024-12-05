db0608b9b6c1ab4bd182c9c91ca7c5f076ae3c28
Tucker Craig <tuckermcraig@gmail.com>
2024-12-04 22:01:11 -0500

    learned some c

---
+++ c-project.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int num = 23;
+    int num2 = 32;
+    float flt = 23.5;
+    char initial = 'T';
+    printf("%i\n%i\n%f\n%c", num, num2, flt, initial); // This prints stuff
+    return 0;
+}
